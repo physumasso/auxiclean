16c4f0ec35793f2494c990af894179f1cd4dd212
fgoudreault <https://fgoudreault@gitlab.com>
2017-05-23 17:01:23 -0400

    Started unittest framework
    - Created first unittest for distributeur

---
+++ auxiclean/__init__.py
@@ -0,0 +1,1 @@
+from .distributeur import Distributeur
---
+++ auxiclean/unittests/test_coding_standards.py
@@ -0,0 +1,30 @@
+import pep8
+import os
+import auxiclean
+from nose.tools import assert_equal
+
+# Add pep8 codes to ignore below
+PEP8_ADDITIONAL_IGNORE = []
+# add files to ignore below
+EXCLUDE_FILES = []
+
+
+def test_pep8_conformance():
+
+    dirs = []
+    dirname = os.path.dirname(auxiclean.__file__)
+    dirs.append(dirname)
+
+    pep8style = pep8.StyleGuide()
+
+    # Extends the number of pep8 guidelines which are not checked
+    pep8style.options.ignore += tuple(PEP8_ADDITIONAL_IGNORE)
+    pep8style.options.exclude.extend(EXCLUDE_FILES)
+
+    result = pep8style.check_files(dirs)
+    msg = "Found code syntax errors (and warnings)!"
+    assert_equal(result.total_errors, 0, msg)
+
+if __name__ == "__main__":
+    import nose
+    nose.runmodule()
