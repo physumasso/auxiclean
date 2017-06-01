59efe605f2e5bd329fee83ce19794854c1282aad
tanjay <tanguay_jeremie@hotmail.com>
2017-05-31 22:20:15 -0400

    Changement de la fonction filtre et de la distribution pour prendre en compte __lt__

--- auxiclean/distributeur.py
+++ auxiclean/distributeur.py
@@ -1,6 +1,6 @@
-from .students import Students
-from .cours import Cours
-
+from students import Students
+from cours import Cours
+import numpy as np
 #Commentaire
 
 class Distributeur:
@@ -25,7 +25,7 @@ class Distributeur:
 
     def _get_cours(self, path):
         liste = []
-        print("\n#####  COURS À COMBLER  #####")
+        print("\n#####  COURS A COMBLER  #####")
         with open(path) as class_data:
             lines = class_data.readlines()
             for line in lines[1:]:
@@ -34,65 +34,16 @@ class Distributeur:
                 liste.append(c)
         return liste
 
-    def find_best(self, stat_1, stat_2):
-        if stat_1 > stat_2:
-            return True
-        if stat_1 == stat_2:
-            return None
-        else:
-            return False
+    def Filtre(self, nombre_de_poste, liste_de_candidats, liste_des_etudiants):
 
-    def bestmatch(self, nombre_de_poste, liste_de_candidats, nom_du_cours,
-                  liste_des_etudiants):
+        np.sort(liste_de_candidats)
+        Candidats_Choisis = liste_de_candidats[:nombre_de_poste]
+        Candidats_Refuses = liste_de_candidats[nombre_de_poste:]
 
-        classement = []
-        personnes_selectionnees = []
+        for Candidats in Candidats_Refuses :
+            Candidats.dispos = str(int(Candidats.dispos) + 1)
 
-        for candidats in liste_de_candidats:
-            he_is_placed = False
-            if not len(classement):
-                classement.append(candidats)
-            else:
-                index = 0
-                for person in classement:
-                    if he_is_placed is False:
-                        for i in range(1, 7):
-                            if he_is_placed is False:
-                                results = self.find_best(candidats[i],
-                                                         person[i])
-                                if results is True:
-                                    classement.insert(index, candidats)
-                                    he_is_placed = True
-                                if results is False:
-                                    break
-                                if results is None:
-                                    if i == 6:
-                                        string = ("Pour le cours %s,"
-                                                  " choisir le"
-                                                  " gagnant entre 1 : %s et"
-                                                  " 2 : %s : " % (nom_du_cours,
-                                                                  candidats[0],
-                                                                  person[0]))
-                                        egalite = input(string)
-                                        print("Vous avez choisi %s" % egalite)
-                                        if egalite == 1:
-                                            classement.insert(index, candidats)
-                                            he_is_placed = True
-                        index = index + 1
-                if he_is_placed is False:
-                    classement.append(candidats)
-
-        rang = 0
-
-        for person in classement:
-            if rang < nombre_de_poste:
-                personnes_selectionnees.append(person)
-            else:
-                for etudiant in liste_des_etudiants:
-                    if etudiant.name == person[0]:
-                        etudiant.dispos = str(int(etudiant.dispos) + 1)
-            rang += 1
-        return personnes_selectionnees
+        return(Candidats_Choisis)
 
     def make_distribution(self, liste_des_cours, liste_des_etudiants):
         while True:
@@ -103,31 +54,27 @@ class Distributeur:
                         if students.dispos != "0":
                             changement = 1
                             students.dispos = str(int(students.dispos) - 1)
-                            cours.etudiants.append([students.name,
-                                                    int(students.choix[0][1]),
-                                                    int(students.tp),
-                                                    int(students.scolarite),
-                                                    int(students.nobel),
-                                                    int(students.programme),
-                                                    int(students.cote)])
+                            cours.etudiants.append(students)
                             students.choix.pop(0)
 
                 if (len(cours.etudiants)) > int(cours.dispos):
-                    best = self.bestmatch(int(cours.dispos),
-                                          cours.etudiants,
-                                          cours.name,
-                                          liste_des_etudiants)
-                    cours.etudiants = []
-                    for personne in best:
-                        cours.etudiants.append(personne)
+                    Candidats_Choisis = self.Filtre(int(cours.dispos),
+                                            cours.etudiants,
+                                            liste_des_etudiants)
+                    cours.etudiants = Candidats_Choisis
             if changement == 0:
                 break
         distribution = {}
         for cours in liste_des_cours:
-            distribution[cours.name] = [e[0] for e in cours.etudiants]
+            distribution[cours.name] = [e.name for e in cours.etudiants]
         return distribution
 
     def print_distribution(self):
         print("\n#####  DISTRIBUTION  #####")
         for cours, liste_tpistes in self.distribution.items():
             print("%s : %s" % (cours, str(liste_tpistes)))
+
+if __name__ == "__main__":
+    path_students = "../examples/students.csv"
+    path_cours = "../examples/cours.csv"
+    Distributeur(path_students, path_cours)
