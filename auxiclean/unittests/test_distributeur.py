8c3603bed57f758382fe53884879ae8715370351
fgoudreault <https://fgoudreault@gitlab.com>
2017-06-07 09:45:34 -0400

    Added a test for the sorting procedure (without equality).

--- auxiclean/unittests/test_distributeur.py
+++ auxiclean/unittests/test_distributeur.py
@@ -39,8 +39,10 @@ class TestBase(unittest.TestCase):
 
 
 class TestDistributeur(TestBase):
+    # two different courses
     cours = {"Electro": ["1", "1", "1"],
              "Astro": ["10", "1", "1"]}
+    # two candidates each applying for a different course. No conflict
     candidatures = {"Albert A": ["101", "403", "0", "0", "0", "2",
                                  "6", "2", "0", "2", "3"],
                     "Claude C": ["210", "211", "0", "0", "0", "2", "3", "3",
@@ -52,3 +54,22 @@ class TestDistributeur(TestBase):
         dist = self.distributeur.distribution
         self.assertEqual(dist["Electro"][0], "Albert A")
         self.assertEqual(dist["Astro"][0], "Claude C")
+        # check that there is no other candidates in the distribution
+        self.assertEqual(len(dist["Electro"]), 1)
+        self.assertEqual(len(dist["Astro"]), 1)
+
+
+class TestSortingDistributeur(TestBase):
+    # One course with one place
+    cours = {"Electro": ["1", "1", "1"]}
+    # two candidates apply for the same course but Alice gave it one time
+    # So Alice has more experience than Bob, she should win the position.
+    candidatures = {"Bob": ["001", "0", "0", "0", "0", "1",
+                            "0", "1", "0", "2", "3"],
+                    "Alice": ["101", "0", "0", "0", "0", "1",
+                              "1", "1", "0", "2", "3"]}
+
+    def test_sort_by_specific_tp_experience(self):
+        # results
+        dist = self.distributeur.distribution
+        self.assertEqual(dist["Electro"][0], "Alice")
