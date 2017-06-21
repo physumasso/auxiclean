7cdc007d086e334f454533573c703911d586fe16
fgoudreault <felix.goudreault82@gmail.com>
2017-06-21 16:30:20 -0400

    Added checkups for excel files completion and candidate/course initialization

--- auxiclean/managers/excel_manager.py
+++ auxiclean/managers/excel_manager.py
@@ -7,20 +7,17 @@ class ExcelFileManager:
     def __init__(self, path):
         self.path = path
         self.wb = load_workbook(path)
-        self._courses = None
-        self._candidates = None
+        self._courses = CoursesSheetManager(self.wb)
+        self._candidates = CandidatesSheetManager(self.wb)
+        self._checkup()
         self._distribution = None
 
     @property
     def courses(self):
-        if self._courses is None:
-            self._courses = CoursesSheetManager(self.wb)
         return self._courses.courses
 
     @property
     def candidates(self):
-        if self._candidates is None:
-            self._candidates = CandidatesSheetManager(self.wb)
         return self._candidates.candidates
 
     def write_distribution(self, distribution):
@@ -31,3 +28,16 @@ class ExcelFileManager:
 
     def save(self):
         self.wb.save(self.path)
+
+    def _checkup(self):
+        for candidate in self.candidates:
+            for choice in candidate.choices:
+                if not self._choice_in_courses(choice):
+                    raise ValueError(" Choice %s of %s not in courses list." %
+                                     (choice, candidate))
+
+    def _choice_in_courses(self, choice):
+        for course in self.courses:
+            if course.code == choice:
+                return True
+        return False
