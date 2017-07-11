bd7011bc28b9e6fc2090aaa23421b53f035030a5
fgoudreault <felix.goudreault82@gmail.com>
2017-07-11 14:17:19 -0400

    Pimping GUI
    - Added buttons, textboxes, logboxes to main GUI frame.
    - Added second GUI frame for manual selection when there is equalities.
      User needs to check the boxes of which candidate to take.
    - Added logging objects everywhere instead of prints. All loggings goes
      into the main GUI logbox. This is done via the handler.

--- auxiclean/__init__.py
+++ auxiclean/__init__.py
@@ -1,5 +1,17 @@
-from .selector import Selector
 from ._version import get_versions
+import logging
+
 
 __version__ = get_versions()['version']
 del get_versions
+
+
+# module level logger
+fmt = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+logging.basicConfig(level=logging.DEBUG, format=fmt)
+MAINFORMATTER = logging.Formatter(fmt)
+MAINLOGGER = logging.getLogger("auxiclean")
+
+
+# put this at the end to prevent import loop
+from .selector import Selector  # noqa
