da00fa1e3bb3b19fc5029ec94f36eaa2bb9042b2
fgoudreault <felix.goudreault82@gmail.com>
2017-07-18 16:02:57 -0400

    Updating setup develop requirements
    and fixed setup for windows

--- setup.py
+++ setup.py
@@ -4,11 +4,20 @@ import sys
 import versioneer
 
 
+def win_pip_install(package, install_list):
+    if package not in install_list:
+        return
+    print("Installing %s using pip instead of setuptools." % package)
+    pip.main(["install", package])
+    install_list.remove(package)
+
+
 # package required
 install_packages = ["openpyxl",  # to manage excel spreadsheets
+                    "cx_freeze",  # to create executable file on windows
                     ]
 
-develop_packages = ["nose", "pep8", "coverage"]
+develop_packages = ["nose", "pep8", "coverage", "coveralls"]
 print("Installing auxiclean, the following packages are required:",
       install_packages)
 
@@ -17,11 +26,12 @@ if "develop" in sys.argv:
            "  purposes:"), develop_packages)
     install_packages += develop_packages
 
-# if on windows, there is a bug when installing coverage with setuptools.
-# installing it with pip before setup fixes this.
-if sys.platform == "win32" and "coverage" in install_packages:
-    pip.main(["install", "coverage"])
-    install_packages.remove("coverage")
+# if on windows, there is a bug when installing some packages with setuptools.
+# installing them with pip before setup fixes this.
+problematics = ("cx_freeze", "coverage")
+if sys.platform == "win32":
+    for problematic in problematics:
+        win_pip_install(problematic, install_packages)
 
 setup(name="auxiclean",
       version=versioneer.get_version(),
