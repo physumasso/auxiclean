868d7a8a5169ccd12c556e6ac1b8820f9a07f4dd
fgoudreault <felix.goudreault82@gmail.com>
2017-08-23 18:29:28 -0400

    Adding more checkups to make sure everything is ok before Run

--- auxiclean/managers/excel_manager.py
+++ auxiclean/managers/excel_manager.py
@@ -1,6 +1,6 @@
 from openpyxl import load_workbook
 from .sheet_managers import (CoursesSheetManager, CandidatesSheetManager,
-                             DistributionSheetManager)
+                             DistributionSheetManager, ExcelError)
 
 
 class ExcelFileManager:
@@ -30,11 +30,51 @@ class ExcelFileManager:
         self.wb.save(self.path)
 
     def _checkup(self):
+        all_courses_disciplines = []
+        all_candidates_disciplines = []
+        # check that all candidates choices exists in the courses list
         for candidate in self.candidates:
+            if candidate.discipline not in all_candidates_disciplines:
+                all_candidates_disciplines.append(candidate.discipline.lower())
             for choice in candidate.choices:
                 if not self._choice_in_courses(choice):
-                    raise ValueError(" Choice %s of %s not in courses list." %
-                                     (choice, candidate))
+                    raise ExcelError("Le choix %s de %s n'est pas dans la"
+                                     " liste de cours." %
+                                     (choice, candidate.name))
+
+        # check for duplicate candidates
+        for j, candidate in enumerate(self.candidates):
+            for candidate2 in self.candidates[j + 1:]:
+                if candidate.name == candidate2.name:
+                    raise ExcelError("Candidature %s dupliquée." %
+                                     candidate.name)
+
+        # check for duplicate courses
+        for j, course in enumerate(self.courses):
+            if course.discipline not in all_courses_disciplines:
+                all_courses_disciplines.append(course.discipline.lower())
+            for course2 in self.courses[j + 1:]:
+                if course.code == course2.code:
+                    raise ExcelError("Cours %s dupliqué." % course.code)
+
+        # check for disciplines
+        for candidate in self.candidates:
+            d = candidate.discipline.lower()
+            if d is None or d.lower() == "générale":
+                continue
+            if d not in all_courses_disciplines:
+                raise ExcelError("La discipline '%s' de %s n'est pas dans"
+                                 " la liste de toutes les"
+                                 " disciplines des cours." %
+                                 (d, candidate.name))
+        for course in self.courses:
+            d = course.discipline.lower()
+            if d is None or d.lower() == "générale":
+                continue
+            if d not in all_candidates_disciplines:
+                raise ExcelError("La discipline '%s' du cours %s n'es pas dans"
+                                 " la liste de toutes les disciplines des"
+                                 " candidatures." % (d, course.code))
 
     def _choice_in_courses(self, choice):
         for course in self.courses:
