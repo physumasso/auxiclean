5b09171bf20ea743cb104feab1d0ab7ee5cbad34
Félix Antoine Goudreault <felix.goudreault82@gmail.com>
2017-12-02 21:59:10 -0500

    Modified GUI to take into account parameters
    - Added a parameter modificator GUI to change the run time parameters.
    - Modified Main window appearance.
    - The excel open file warning window has been reduced to a simple
    message box.
    - Base class implementation to make it easier to implement another
    window popup from the main window.

--- auxiclean/gui/browser.py
+++ auxiclean/gui/browser.py
@@ -1,8 +1,9 @@
-from ..selector import Selector
 from ..handler import TextHandler
 from tkinter import filedialog, scrolledtext
-from auxiclean import MAINLOGGER
+from auxiclean import MAINLOGGER, Selector
 from timeit import default_timer as timer
+from .settings_gui import SettingsGUI
+from ..managers import ConfigManager
 import auxiclean
 import tkinter as tk
 import os
@@ -18,10 +19,13 @@ class Browser:
         self.frame.grid(sticky=tk.E + tk.W)
         self.master.title("Auxiclean - GUI - version: %s" %
                           auxiclean.__version__)
+        # load config
+        self.config_manager = ConfigManager()
         # browser logger
-        self.logger = logging.getLogger("auxiclean.browser")
+        logging.basicConfig()
+        self._logger = logging.getLogger("auxiclean.browser")
+        self._logger.setLevel(self.config_manager.loglevel)
         self.createMainWindow()
-        self.frame.pack()
 
     def createMainWindow(self):
         top = self.frame.winfo_toplevel()
@@ -35,6 +39,8 @@ class Browser:
         self.labelPathBox.grid(column=0)
         self.pathBox.grid(column=1, row=0,
                           sticky=tk.E + tk.W)
+        # insert last excel file used
+        self.pathBox.insert(0, self.config_manager.last_excel_file)
         # create browse button
         self.browseButton = tk.Button(self.frame, text="Chercher",
                                       command=self.select_file)
@@ -42,7 +48,11 @@ class Browser:
         # create run button
         self.runButton = tk.Button(self.frame, text="Exécution",
                                    command=self.run_selector)
-        self.runButton.grid()
+        self.runButton.grid(row=1, column=0)
+        # creates settings button
+        self.settingsButton = tk.Button(self.frame, text="Paramètres",
+                                        command=self.settings_config)
+        self.settingsButton.grid(row=1, column=2)
         # create quit button
         self.quitButton = tk.Button(self.frame, text="Quitter",
                                     command=self.frame.quit)
@@ -50,11 +60,11 @@ class Browser:
         # create DEBUG button (for verbose option in GUI)
         self.debugCheckButton = tk.Variable()
         _actual_button = tk.Checkbutton(self.frame,
-                                        text="Full traceback (DEBUG)",
+                                        text=" Full traceback (DEBUG)",
                                         anchor="e",
                                         variable=self.debugCheckButton)
         _actual_button.deselect()
-        _actual_button.grid(row=2, column=2)
+        _actual_button.grid(row=2, column=1, columnspan=2)
 
         # create log box
         # adapted from this SO post:
@@ -62,7 +72,7 @@ class Browser:
         # Add text widget to display logging info
         self.logBox = scrolledtext.ScrolledText(self.frame, state='disabled')
         self.logBox.configure(font='TkFixedFont')
-        self.logBox.grid(sticky='w', columnspan=3)
+        self.logBox.grid(sticky=tk.W + tk.E + tk.S, columnspan=3)
 
         # Create textLogger
         self.text_handler = TextHandler(self.logBox)
@@ -74,55 +84,67 @@ class Browser:
                                    text="Il y a un bug? Écrivez le ici!",
                                    fg="blue",
                                    cursor="hand2")
-        self.bug_report.grid(column=0)
+        self.bug_report.grid(column=0, sticky=tk.W + tk.S, columnspan=3)
         self.bug_report.bind("<Button-1>", self.bug_callback)
 
     def bug_callback(self, event):
         webbrowser.open_new(r"https://github.com/physumasso/auxiclean/issues")
 
+    def settings_config(self):
+        newWindow = tk.Toplevel(self.master)
+        SettingsGUI(self.config_manager, newWindow,
+                    loglevel=self.config_manager.loglevel)
+        self.master.wait_window(newWindow)
+        # settup new loglevel if changed
+        self._logger.setLevel(self.config_manager.loglevel)
+
     def run_selector(self):
         path = self.pathBox.get()
         if not len(path):
-            self.logger.error("Veuiller entrer un fichier Excel.")
+            self._logger.error("Veuiller entrer un fichier Excel.")
             return
 
         path = os.path.abspath(path)
         if not os.path.exists(path):
-            self.logger.error("%s n'existe pas!" % path)
+            self._logger.error("%s n'existe pas!" % path)
             return
         if not os.path.isfile(path):
-            self.logger.error("%s n'est pas un fichier." % path)
+            self._logger.error("%s n'est pas un fichier." % path)
             return
         if not path.endswith(".xlsx") and not path.endswith(".ods"):
-            self.logger.error("%s doit être un fichier excel ou ods." % path)
+            self._logger.error("%s doit être un fichier excel ou ods." % path)
             return
+        # path is good, save it for next time user open program
+        self.config_manager.last_excel_file = path
+        self.config_manager.write_config()
 
         start = None
         try:
             # warn user to close the excel file
             self.warn_user_excel(path)
-            self.logger.info("Exécution ... ceci peut prendre"
-                             " quelques minutes ...")
+            self._logger.info("Exécution ... ceci peut prendre"
+                              " quelques minutes ...")
             # time process
             start = timer()
-            Selector(path, master=self.master)
+            Selector(path, master=self.master,
+                     loglevel=self.config_manager.loglevel)
         except Exception as e:
             if not int(self.debugCheckButton.get()):
-                self.logger.error("%s" % e)
+                self._logger.error("%s" % e)
             else:
                 # print full traceback
                 tb = traceback.format_exc()
-                self.logger.error(tb)
-            self.logger.info("STOP - ERREUR")
+                self._logger.error(tb)
+            self._logger.info("STOP - ERREUR")
         else:
-            self.logger.info("Succès!")
+            self._logger.info("Succès!")
         finally:
             end = timer()
             if start is None:
                 # an error occured before the call to the selector
                 return
-            self.logger.info("Temps d'exécution = %.3f secondes" %
-                             (end - start))
+            self._logger.info("Temps d'exécution = %.3f secondes" %
+                              (end - start))
 
     def select_file(self):
         data_file = filedialog.askopenfile(parent=self.master,
@@ -131,47 +153,20 @@ class Browser:
         if data_file is not None:
             path = data_file.name
             data_file.close()
+        else:
+            return
         # delete previous text
-        l = len(self.pathBox.get())
-        self.pathBox.delete(0, last=l)
+        text = len(self.pathBox.get())
+        self.pathBox.delete(0, last=text)
         self.pathBox.insert(0, path)
 
     def warn_user_excel(self, path):
-        # create new window
-        newWindow = tk.Toplevel(self.master)
-        WarnExcelWindow(path, master=newWindow)
-        self.master.wait_window(newWindow)
-
-
-class WarnExcelWindow:
-    def __init__(self, path, master):
-        self.master = master
-        self.frame = tk.Frame(self.master)
-        self.master.title("Attention")
-        # logging config
-        self.logger = logging.getLogger("auxiclean.gui.WarnExcelWindow")
-        self.create_window(path)
-        self.frame.pack()
-
-    def create_window(self, path):
-        top = self.frame.winfo_toplevel()
-        top.rowconfigure(0, weight=1)
-        top.columnconfigure(0, weight=1)
-        self.frame.rowconfigure(0, weight=1)
-        self.frame.columnconfigure(1, weight=1)
+        if not self.config_manager.excel_file_open_warning:
+            return
         text = ("Attention!\n\n Veuillez vous assurer que \n"
                 " la fenêtre Excel (ou"
                 " OpenOffice) du"
                 " fichier suivant est fermée:\n\n %s \n\n"
                 "Si ce n'est pas le cas, la distribution ne pourra\n"
                 " pas être écrite dans le fichier." % path)
-        self.text = tk.Label(self.frame, text=text, anchor=tk.W,
-                             justify=tk.CENTER)
-        self.text.grid(column=0, row=0)
-        self.okButton = tk.Button(self.frame, text="OK",
-                                  command=self.quit)
-        self.okButton.grid()
-
-    def quit(self):
-        self.master.destroy()
-        return
+        tk.messagebox.showinfo("Attention", text)
