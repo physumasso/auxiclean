00f7ee0a69fd08541c465386ab8b95e32ebe7dd0
Félix Antoine Goudreault <felix.goudreault82@gmail.com>
2018-03-13 15:48:23 -0400

    Modified deploy method
    - created bundle.py file that executes the commands to bundle the app
    depending of the OS.
    - Modified yml files for CI to use this file instead of specific scripts
    for each OS.

---
+++ bundle.py
@@ -0,0 +1,36 @@
+import os
+import shutil
+
+
+# remove these directories if they exists for a clean bundle.
+to_remove = ("build", "dist")
+
+for remove in to_remove:
+    if os.path.exists(remove):
+        print(f"Deleting existing '{remove}' directory for clean bundling.")
+        shutil.rmtree(remove)
+
+
+osx_command = ("pyinstaller --onefile --windowed --clean --name=auxiclean_osx"
+               " run.py; cd dist; zip -r auxiclean_osx.zip auxiclean_osx.app;"
+               " cd ..")
+linux_command = ("pyinstaller --onefile --windowed --clean --name=auxiclean"
+                 "_linux run.py")
+windows_command = ("pyinstaller --onefile --windowed --name=auxiclean_windows"
+                   " run.py")
+
+# we probably should not use os.system calls.
+# TODO: change os.system to subprocess.Popen...
+
+platform = os.sys.platform
+if platform == "linux":
+    print("Bundling for linux.")
+    os.system(linux_command)
+elif platform == "darwin":
+    print("Bundling for OSX.")
+    os.system(osx_command)
+elif platform == "windows":
+    print("Bundling for Windows.")
+    os.system(windows_command)
+else:
+    raise OSError(f"OS '{platform}' not recognized... cannot bundle app.")
